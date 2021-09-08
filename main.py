f82983c51f49bd79d455fb613509ff9193654565
Patty <L224749391@gmail.com>
2021-09-08 23:07:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=55001)
+    app.run(host="0.0.0.0", port=8501)
