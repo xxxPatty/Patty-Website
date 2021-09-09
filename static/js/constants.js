50d4b1efbf7f3c66af02e6b9733a2c8c93afb26e
Patty <L224749391@gmail.com>
2021-09-10 05:13:30 +0800

    url

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,4 @@
 const action_name = "action_hello_world";
 const rasa_server_url = "http://ec2-18-218-253-152.us-east-2.compute.amazonaws.com:5005/webhooks/rest/webhook";
 const sender_id = uuidv4();
+const head_url = "http://ec2-18-218-253-152.us-east-2.compute.amazonaws.com";
